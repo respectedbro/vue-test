3bd728926dbcd932632e351b71a65f1935940af4
NikDm <respectedoff@yandex.ru>
2024-10-25 21:38:22 +0300

    test 3

--- vue-test-3/app.js
+++ vue-test-3/app.js
@@ -1,12 +1,30 @@
 Vue.createApp({
-    data: () => ({
-        myHtml: '<h1>Vue 3 app</h1>',
-        title: 'Я есть Грут',
-        person: {
-            firstName: 'Nik',
-            lastName: 'Dm',
-            age: 35
-        },
-        items: [1, 2, 3, 4, 5, 6]
-    })
+   data: () => ({
+      myHtml: '<h1>Vue 3 app</h1>',
+      title: 'Я есть Грут',
+      person: {
+         firstName: 'Nik',
+         lastName: 'Dm',
+         age: 35
+      },
+      items: [1, 2]
+   }),
+   methods: {
+      addItem() {
+         this.items.unshift(this.$refs.myInput.value)
+         this.$refs.myInput.value
+         console.log(event.key)
+      },
+      remove(i) {
+         this.items.splice(i, 1)
+      },
+      log(item) {
+         console.log('log item', item)
+      }
+   },
+   computed: {
+      evenItems() {
+         return this.items.filter(i => i % 2 === 0)
+      }
+   }
 }).mount('#app')
