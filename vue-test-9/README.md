7c2c8a7b9daaa247b2d6699b12ddf92ba542b566
NikDm <respectedoff@yandex.ru>
2024-11-03 21:06:59 +0300

    15min

---
+++ vue-test-9/README.md
@@ -0,0 +1,7 @@
+# 09-vue-reusability
+
+// $env:NODE_OPTIONS="--openssl-legacy-provider"
+// npm run serve
+
+Миксины Директивы плагины
+
---
+++ vue-test-9/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <app-alert
+        v-if="alert"
+        text="It's really important message"
+        title="Warning!"
+        type="danger"
+        closable
+        @close="alert = false"
+    ></app-alert>
+
+    <div class="card">
+      <button class="btn primary" @click="toggleAlert">{{alert ? 'Скрыть' : 'Показать'}} сообщение</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppAlert from "./components/AppAlert.vue";
+
+export default {
+  components: {AppAlert},
+
+  data() {
+    return {
+      alert: false,
+    }
+  },
+  methods: {
+    toggleAlert() {
+      this.alert = !this.alert
+    }
+  }
+}
+</script>
+
