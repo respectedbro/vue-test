698af0f9d0e13f2464cf5b72b527f98c1e5446cc
NikDm <respectedoff@yandex.ru>
2024-11-03 18:29:46 +0300

    stac

---
+++ vue-min-test-8/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container column">
+    <ResumeForm @block-added="addBlock"/>
+    <ResumeView :blocks="blocks" />
+
+  </div>
+  <div class="container">
+    <p>
+      <button class="btn primary">Загрузить комментарии</button>
+    </p>
+    <div class="card">
+      <h2>Комментарии</h2>
+      <ul class="list">
+        <li class="list-item">
+          <div>
+            <p><strong>test@microsoft.com</strong></p>
+            <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, reiciendis.</small>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="loader"></div>
+  </div>
+</template>
+
+<script>
+import ResumeForm from "@/components/ResumeForm.vue";
+import ResumeView from "@/components/ResumeView.vue";
+
+export default {
+  components: {ResumeView, ResumeForm},
+  data() {
+    return {
+      blocks: [],
+      comments: [],
+      loading: false
+    }
+  },
+  methods: {
+    addBlock(block) {
+      this.blocks.push(block)
+    }
+  }
+}
+</script>
+
+<style>
+.avatar {
+  display: flex;
+  justify-content: center;
+}
+
+.avatar img {
+  width: 150px;
+  height: auto;
+  border-radius: 50%;
+}
+</style>
---
+++ vue-min-test-8/src/components/ResumeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card card-w70">
+    <template v-if="blocks.length">
+      <component
+          v-for="block in blocks"
+          :key="block.id"
+          :is="'resume-' + block.type"
+          v-bind="{value: block.value}"
+      ></component>
+    </template>
+    <h3 v-else>Добавьте первый блок, чтобы увидеть результат</h3>
+  </div>
+</template>
+
+<script>
+import ResumeTitle from "@/components/parts/ResumeTitle.vue";
+export default {
+  components: {ResumeTitle},
+  props: ['blocks'],
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
