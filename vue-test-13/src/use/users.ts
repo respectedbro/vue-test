4a83728e7368cb31f7db49e0b0d43179f1d8bb5e
resAsus <respectedbro@yandex.ru>
2024-11-21 19:23:29 +0300

    typeScript vue

---
+++ vue-test-13/src/use/users.ts
@@ -0,0 +1,26 @@
+import {ref, Ref} from 'vue'
+import {useFetch} from "@/use/fetch"
+
+
+interface User {
+    id: number,
+    name: string,
+    email: string
+}
+
+type UsableUsers = Promise<{ users: Ref<User[] | undefined> }>
+
+export async function useUsers(): UsableUsers {
+    const loading = ref(false)
+
+    const {response: users, request} = useFetch<User[]>(
+        'https://jsonplaceholder.typicode.com/users'
+    )
+
+    if (!loading.value) {
+        await request()
+        loading.value = true
+    }
+
+    return {users}
+}
