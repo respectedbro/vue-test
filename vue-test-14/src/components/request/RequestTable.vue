4384274d1ff69ec91d825419f464dda9bff9f186
resAsus <respectedbro@yandex.ru>
2024-11-28 17:45:20 +0300

    modal req

--- vue-test-14/src/components/request/RequestTable.vue
+++ vue-test-14/src/components/request/RequestTable.vue
@@ -29,7 +29,6 @@
 
 export default {
   props: ['requests']
-
 }
 </script>
 
--- vue-test-14/src/views/Home.vue
+++ vue-test-14/src/views/Home.vue
@@ -1,20 +1,37 @@
 <template>
   <app-page title="Список заявок">
     <template #header>
-      <button class="btn primary">Создать</button>
+      <button class="btn primary" @click="modal = true">Создать</button>
     </template>
 
 
     <request-table :requests="[]"></request-table>
+
+
+    <teleport to="body">
+      <app-modal v-if="modal" title="Создать заявку" @close="modal = false">
+        <request-modal/>
+      </app-modal>
+    </teleport>
   </app-page>
 
 </template>
 
 <script>
+import {ref} from 'vue'
 import AppPage from "../components/ui/AppPage.vue";
 import RequestTable from "../components/request/RequestTable.vue";
+import RequestModal from "@/components/request/RequestModal.vue";
+import AppModal from "@/components/ui/AppModal.vue";
 
 export default {
-  components: {AppPage, RequestTable}
+  components: {AppPage, RequestTable, AppModal, RequestModal},
+  setup() {
+    const modal = ref(false)
+
+    return {
+      modal,
+    }
+  }
 }
 </script>
